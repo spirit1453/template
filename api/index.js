e3a863e06cb39e2d3a00551b1022a4c5552252df
spirit <catchspirit37@gmail.com>
2018-07-30 16:27:02 +0800

    zz

--- api/index.js
+++ api/index.js
@@ -1,15 +1,13 @@
+
 const express = require('express')
 
-// Create express instnace
 const app = express()
 
-// Require API routes
 const users = require('./routes/users')
 
-// Import API Routes
 app.use(users)
 
-// Export the server middleware
+
 module.exports = {
   path: '/api',
   handler: app
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,17 @@
 <template>
   <section class="container">
-    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
-    <h1 class="title">
-      USERS
-    </h1>
-    <ul class="users">
-      <li v-for="(user, index) in users" :key="index" class="user">
-        <nuxt-link :to="{ name: 'id', params: { id: index }}">
-          {{ user.name }}
-        </nuxt-link>
-      </li>
-    </ul>
+        {{data}}
   </section>
 </template>
 
 <script>
-import axios from '~/plugins/axios'
-
+// import axios from '~/plugins/axios'
 export default {
   async asyncData () {
-    let { data } = await axios.get('/api/users')
-    return { users: data }
+     const result = await new Promise(resolve=>{
+         resolve ({ data:"test" })
+     })
+    return result
   },
   head () {
     return {
@@ -31,18 +22,5 @@ export default {
 </script>
 
 <style scoped>
-.title
-{
-  margin: 30px 0;
-}
-.users
-{
-  list-style: none;
-  margin: 0;
-  padding: 0;
-}
-.user
-{
-  margin: 10px 0;
-}
+
 </style>
