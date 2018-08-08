9a06865c9b121e87d5c740b5c3329f73cbb2289e
spirit <catchspirit37@gmail.com>
2018-08-08 09:47:13 +0800

    z

--- bin/scaffold.js
+++ bin/scaffold.js
@@ -5,7 +5,40 @@ const {argv} = yargs
 const {index} = argv
 const path = require('path')
 const rootPath = path.resolve(__dirname,'../')
+const boilerplateFolder = path.resolve(rootPath,'boilerplate')
 const exampleFolder = path.resolve(rootPath,'example')
-const indexFolder = path.resolve(exampleFolder,index)
+const fse = require('fs-extra')
+const fs = require('fs')
+const inquirer = require('inquirer')
 
-fse.ensureDirSync(indexFolder)
+const standard = {
+    index:{
+        type: 'list',
+        message: 'which index of the template?',
+        choices: fs.readdirSync(boilerplateFolder).filter(ele=>{
+            return !ele.startsWith('.')
+        }),
+    }
+}
+
+const question = []
+
+for(let key in standard){
+    const value = standard[key]
+    value.name = key
+    question.push(value)
+}
+
+(async()=>{
+    const answer = await inquirer.prompt(question)
+    console.log(JSON.stringify(answer, null, '  '));
+    const {index} = answer
+    const source = path.resolve(boilerplateFolder,index)
+    const destinaiton = path.resolve(exampleFolder,index)
+
+    fse.ensureDirSync(destinaiton)
+    fse.copySync(source,destinaiton)
+    console.log('end')
+
+})()
+
---
+++ example/1/_meta/introduction.md
@@ -0,0 +1,12 @@
+
+# description
+    basic bare configration for a node project,other node project templates are based on
+    this template
+
+# features
+    1. eslint
+    2. flow
+    3. gulp
+    4. most used dependency and devDependency like uuid yargs
+
+
