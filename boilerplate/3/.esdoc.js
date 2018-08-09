825c1f95a40fdadeede73a586696aa70262cba8e
spirit <catchspirit37@gmail.com>
2018-08-09 11:01:17 +0800

    z

---
+++ boilerplate/3/.esdoc.js
@@ -0,0 +1,13 @@
+
+
+
+const esdoc = {
+    "source": "./src",
+    "destination": "./docs",
+    "plugins": [
+        {"name": "esdoc-standard-plugin"}
+    ]
+}
+
+Object.freeze(esdoc)
+module.exports = esdoc
---
+++ boilerplate/3/_meta/introduction.md
@@ -0,0 +1,6 @@
+
+# description
+  electron template
+
+# features
+
