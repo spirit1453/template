eb63f58559024dacbd28516410c7c3632c1671cb
spirit <catchspirit37@gmail.com>
2018-08-24 14:05:18 +0800

    z

--- boilerplate/0/.esdoc.js
+++ boilerplate/0/.esdoc.js
@@ -1,12 +1,14 @@
-
-
+const path = require('path')
+const rootPath = path.resolve(__dirname,'')
+const srcFolder = path.resolve(rootPath,'src')
+const docFolder = path.resolve(rootPath,'local/doc')
 
 const esdoc = {
-    "source": "./src",
-    "destination": "./docs",
-    "plugins": [
-        {"name": "esdoc-standard-plugin"}
-    ]
+  "source": srcFolder,
+  "destination": docFolder,
+  "plugins": [
+    {"name": "esdoc-standard-plugin"}
+  ]
 }
 
 Object.freeze(esdoc)
