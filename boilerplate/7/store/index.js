9a9c3d52cc7a31ae3a6fe114950c621ec8a82405
spirit <catchspirit37@gmail.com>
2018-09-14 16:42:36 +0800

    z - Fri Sep 14 2018 16:42:36 GMT+0800 (China Standard Time)

---
+++ boilerplate/7/store/index.js
@@ -0,0 +1,23 @@
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+  state: {
+    //
+  },
+  mutations: {
+    //
+  },
+  actions: {
+    //
+  },
+  modules: {
+    user,
+    app
+  }
+})
+
+const createStore = () => {
+  return store
+}
+
+export default createStore
